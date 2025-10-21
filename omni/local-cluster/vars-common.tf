c9d42e102e592943952f2b2c43dc40115a735705
niklasfrick <niklas.frick@pm.me>
2025-10-21 14:19:10 +0200

    ✨ Moving away from terraform for the local cluster for now

--- omni/local-cluster/vars-common.tf
+++
@@ -1,9 +0,0 @@
-variable "network_gateway_ip" {
-  description = "The IP address of the network gateway"
-  type        = string
-}
-
-variable "dns_domain" {
-  description = "The DNS domain to append to hostnames for local DNS records"
-  type        = string
-}
